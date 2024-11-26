672c14a32100805f9d0275533c10cc47084db906
Dylan H. Morris <dylan@dylanhmorris.com>
2024-11-26 16:22:39 -0500

    Generalize nullable_is_in to nullable_comparison, update tests, use in filter_for_scoring

--- R/utils.R
+++ R/utils.R
@@ -1,22 +1,24 @@
-#' Compare a vector to a set of valid values,
-#' returning a single `TRUE` if the set of valid
-#' values is `NULL`.
+#' Compare two quantities `a` and `b` with a given
+#' `comparison operator`,
+#' returning a single `TRUE` if `b` is
+#' `NULL`.
 #'
 #' Useful for letting `NULL` reflect "all values"
 #' if [dplyr::filter()] calls. Internal function.
 #'
-#' @param vector Vector of values to test
-#' @param valid_values Vector of valid values, possibly
-#' `NULL`
-#' @return A logical vector equal in length to `vector`.
-#' Equivalent to `vector %in% valid_values` if `valid_values`
-#' is not `NULL` and to `\(x) { TRUE }` if
-#' `valid_values` is `NULL`.
-nullable_is_in <- function(vector,
-                           valid_values) {
+#' @param a First set of values for the comparison
+#' @param comparison_operator Comparison operator for the comparison,
+#' as a string, e.g. `"=="`, `">="` `"%in%"`.
+#' @param b Second set of values for the comparison, or `NULL`.
+#' @return A logical vector. Equivalent to `b {comparison_operator} a`
+#' if `b` is not `NULL` and to `TRUE` if `b` is `NULL`.
+nullable_comparison <- function(a,
+                                comparison_operator,
+                                b) {
+  comparison_func <- getFunction(comparison_operator)
   return(
-    if (!is.null(valid_values)) {
-      vector %in% valid_values
+    if (!is.null(b)) {
+      comparison_func(a, b)
     } else {
       TRUE
     }
