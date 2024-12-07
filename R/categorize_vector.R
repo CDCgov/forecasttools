385934de69ec157c0ab78212029edb15445849d2
Dylan H. Morris <dylan@dylanhmorris.com>
2024-12-06 20:56:34 -0500

    Make hubverse tests more platform robust, improve categorize vector function and add tests for it

--- R/categorize_vector.R
+++ R/categorize_vector.R
@@ -6,14 +6,17 @@
 #' break points and labels.
 #'
 #' @param values Vector of values to categorize.
-#' @param break_sets list of vectors of category
-#' breakpoints, one for each entry of `values`,
-#' or a single set of category breakpoints to use
-#' for all `values`.
-#' @param label_sets list of vectors of labels to associate
-#' with the the corresponding breakpoints in `break_sets`,
-#' one for each entry of `break_sets` or a single set of
-#' labels to use for all `break_sets`.
+#' @param break_sets sets of category breakpoints,
+#' with one set for each entry of `values`, as a list of
+#' numeric vectors. Alternatively, a single set of category
+#' breakpoints to use for all values, as a one-entry list
+#' containing a single numeric vector.
+#' @param label_sets sets of labels to associate,
+#' with the corresponding breakpoints in `break_sets`,
+#' with one set for each entry of `values`, as a list of
+#' character vectors. Alternatively, a single set of labels
+#' to use for all values, as a one-entry list containing a
+#' single character vector.
 #' @param include.lowest Passed to [base::cut()]. Default `TRUE`.
 #' @param order Passed to [base::cut()]. Default `TRUE`.
 #' @param right Passed to [base::cut()]. Default `TRUE`.
@@ -29,6 +32,17 @@ categorize_vector <- function(values,
                               order = TRUE,
                               right = TRUE,
                               ...) {
+  n_vals <- length(values)
+
+  checkmate::assert_list(break_sets,
+    types = "numeric"
+  )
+  checkmate::assert_list(label_sets,
+    types = "character"
+  )
+  checkmate::qassert(break_sets, c("l1", glue::glue("l{n_vals}")))
+  checkmate::qassert(label_sets, c("l1", glue::glue("l{n_vals}")))
+
   return(purrr::pmap_vec(
     list(
       x = values,
