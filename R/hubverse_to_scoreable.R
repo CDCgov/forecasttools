a50aa6217bdda584c51c7fff68b5722f58a4e863
Dylan H. Morris <dylan@dylanhmorris.com>
2024-12-12 16:27:45 -0500

    Update vignette, run roxygen

--- R/hubverse_to_scoreable.R
+++ R/hubverse_to_scoreable.R
@@ -13,46 +13,45 @@
 #' as a hubverse-format [`tibble`][tibble::tibble()] produced by
 #' `target_end_date`, `value`, and `horizon`
 #' @param observation_table observations, as a [`tibble`][tibble::tibble()].
-#' @param observed_value_column Name of the column containing
+#' @param obs_value_column Name of the column containing
 #' observed values in the `observed` table, as a string.
 #' Default `"value"`
-#' @param observed_location_column Name of the column containing
+#' @param obs_location_column Name of the column containing
 #' location values in the `observed` table, as a string.
 #' Default `"location"`
-#' @param observed_date_column Name of the column containing
+#' @param obs_date_column Name of the column containing
 #' date values in the `observed` table, as a string.
 #' Default `"reference_date"`
 #' @return A [`data.table`][data.table::data.table()] for scoring,
 #' as the output of [scoringutils::as_forecast_quantile()].
 #' @export
-hub_quantiles_to_scoreable <-
-  function(hubverse_quantile_table,
-           observation_table,
-           observed_value_column = "value",
-           observed_location_column = "location",
-           observed_date_column = "reference_date") {
-    obs <- observation_table |>
-      dplyr::select(
-        location = .data[[observed_location_column]],
-        target_end_date = .data[[observed_date_column]],
-        observed = .data[[observed_value_column]]
-      )
+hub_quantiles_to_scoreable <- function(hubverse_quantile_table,
+                                       observation_table,
+                                       obs_value_column = "value",
+                                       obs_location_column = "location",
+                                       obs_date_column = "reference_date") {
+  obs <- observation_table |>
+    dplyr::select(
+      location = .data[[obs_location_column]],
+      target_end_date = .data[[obs_date_column]],
+      observed = .data[[obs_value_column]]
+    )
 
-    scoreable_table <- hubverse_quantile_table |>
-      dplyr::inner_join(obs,
-        by = c(
-          "location",
-          "target_end_date"
-        )
-      ) |>
-      scoringutils::as_forecast_quantile(
-        predicted = "value",
-        observed = "observed",
-        quantile_level = "output_type_id"
+  scoreable_table <- hubverse_quantile_table |>
+    dplyr::inner_join(obs,
+      by = c(
+        "location",
+        "target_end_date"
       )
+    ) |>
+    scoringutils::as_forecast_quantile(
+      predicted = "value",
+      observed = "observed",
+      quantile_level = "output_type_id"
+    )
 
-    return(scoreable_table)
-  }
+  return(scoreable_table)
+}
 
 
 #' Create a table for scoring hub model
