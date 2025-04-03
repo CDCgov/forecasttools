db10266b734bc5c1d5cd211837be44c3e5a3e97f
Dylan H. Morris <dylan@dylanhmorris.com>
2025-04-03 16:43:26 -0400

    Update constants to refer to hubUtils

--- R/constants.R
+++ R/constants.R
@@ -1,18 +1,37 @@
 #' Internal and user-exposed constants for forecasttools.
 
-#' Names of the standard columns in a hubverse table.
+#' Names of the standard columns for hubverse tables.
+#' Alias for [hubUtils::std_colnames].
 #'
 #' @export
-hubverse_column_names <- c(
+hubverse_std_colnames <- hubUtils::std_colnames
+
+#' Names of additional standard columns for
+#' the CDC FluSight and COVID-19 Forecast Hubs
+#' beyond those in [hubverse_std_colnames].
+cdc_hub_additional_colnames <- c(
   "reference_date",
   "target",
   "horizon",
   "target_end_date",
-  "location",
-  "output_type",
-  "output_type_id",
-  "value"
-)
+  "location"
+) |>
+  purrr::set_names()
+
+#' Names of standard columns for the
+#' the CDC FluSight and COVID-19 Forecast Hubs
+#' @export
+cdc_hub_std_colnames <- c(hubverse_std_colnames, cdc_hub_additional_colnames)
+
+#' Alias for [cdc_hub_std_colnames].
+#' @rdname cdc_hub_std_colnames
+#' @export
+covidhub_std_colnames <- cdc_hub_std_colnames
+
+#' Alias for [cdc_hub_std_colnames].
+#' @rdname cdc_hub_std_colnames
+#' @export
+flusight_std_colnames <- cdc_hub_std_colnames
 
 #' Integer start date of a USA MMWR epiweek in ISO numbering.
 #' Value (7) corresponds to Sunday.
