8a23f03bd7f3a8388901b2170507e88a4b5fa63c
Dylan H. Morris <dylan@dylanhmorris.com>
2025-04-03 16:21:57 -0400

    Add widths_to_qi_table internal helper function and test

--- R/hubverse_to_median_qi.R
+++ R/hubverse_to_median_qi.R
@@ -3,7 +3,7 @@
 #'
 #' These intervals are also called "equal-tailed intervals", and can
 #' be computed whenever both the `x` quantile and the `1 - x` are present.
-#' (yielding an interval of width `(1 - x) / 2`.
+#' (yielding an interval of width `1 - (min(x, 1 - x) / 2)`.
 #'
 #' Checks that all quantile_levels are between 0 and 1, inclusive.
 #'
@@ -48,4 +48,42 @@ get_available_qi <- function(quantile_levels,
     )
 
   return(result)
+}
+
+#' Get a table of quantile intervals ("qi") corresponding to a
+#' vector of interval widths.
+#'
+#' These intervals are also called "equal-tailed intervals", and can
+#' be computed whenever both the `x` quantile and the `1 - x` are present.
+#' (yielding an interval of width `1 - (min(x, 1 - x) / 2)`.
+#'
+#' @param widths Vector of interval widths. All entries
+#' must be between 0 and 1, inclusive. Duplicate entries
+#' are ignored.
+#' @param quantile_tol Round quantile level values to this many
+#' decimal places, to avoid problems with floating point number
+#' equality comparisons. Passed as the `digits` argument to
+#' [base::round()]. Default 10.
+#' @return A table of available intervals, as a
+#' [`tibble`][tibble::tibble()], with columns
+#' `.lower_quantile`, `.upper_quantile`, and `.width`,
+#' that give the bounds and the widths of the requested
+#' intervals.
+widths_to_qi_table <- function(widths,
+                               quantile_tol = 10) {
+  widths <- as.numeric(widths) |>
+    round(digits = quantile_tol) |>
+    unique()
+  checkmate::assert_numeric(widths,
+    lower = 0,
+    upper = 1
+  )
+  qi_table <- tibble::tibble(
+    .lower_quantile = 0.5 - widths / 2,
+    .upper_quantile = 0.5 + widths / 2,
+    .width = widths
+  ) |>
+    dplyr::arrange(.data$.lower_quantile)
+
+  return(qi_table)
 }
--- tests/testthat/test_hubverse_to_median_qi.R
+++ tests/testthat/test_hubverse_to_median_qi.R
@@ -35,3 +35,42 @@ test_that("get_available_qi works as expected", {
     )
   )
 })
+
+
+test_that("widths_to_qi_table works as expected", {
+  expect_error(
+    widths_to_qi_table(c(0.25, 0.5, 0.75, 1.01)),
+    "<= 1"
+  )
+  expect_error(
+    widths_to_qi_table(c(0.25, 0.5, 0.75, -0.25)),
+    ">= 0"
+  )
+
+  expect_equal(
+    widths_to_qi_table(c(0.5, 0.50, 0.5000)),
+    tibble::tibble(
+      .lower_quantile = 0.25,
+      .upper_quantile = 0.75,
+      .width = 0.5
+    )
+  )
+
+  expect_equal(
+    widths_to_qi_table(c(0, 1)),
+    tibble::tibble(
+      .lower_quantile = c(0, 0.5),
+      .upper_quantile = c(1, 0.5),
+      .width = c(1, 0)
+    )
+  )
+
+  expect_equal(
+    widths_to_qi_table(c(0.8, 0.8, 0.80, 0.6, 0.60, 1)),
+    tibble::tibble(
+      .lower_quantile = c(0, 0.1, 0.2),
+      .upper_quantile = c(1, 0.9, 0.8),
+      .width = c(1, 0.8, 0.6)
+    )
+  )
+})
