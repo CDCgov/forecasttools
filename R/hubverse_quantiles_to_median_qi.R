fe1d2faf980773341606e4d8688ced10869ed321
Dylan H. Morris <dylan@dylanhmorris.com>
2025-04-07 19:05:11 -0400

    Fix example

--- R/hubverse_quantiles_to_median_qi.R
+++ R/hubverse_quantiles_to_median_qi.R
@@ -18,8 +18,8 @@
 #'
 #' @export
 #' @examples
-#' get_available_qi(c(0.25, 0.3, 0.5, 0.75, 0.99))
-#' get_available_qi(c(0.1, 0.2, 0.5, 0.8, 0.9))
+#' get_available_qi_widths(c(0.25, 0.3, 0.5, 0.75, 0.99))
+#' get_available_qi_widths(c(0.1, 0.2, 0.5, 0.8, 0.9))
 get_available_qi_widths <- function(quantile_levels,
                                     quantile_tol = 10) {
   quantile_levels <- as.numeric(quantile_levels) |>
