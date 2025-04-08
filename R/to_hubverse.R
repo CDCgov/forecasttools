c1aa065ab417bce48eb259d974d551484e5cdc51
Damon Bayer <xum8@cdc.gov>
2025-04-08 14:14:40 -0500

    `to_hubverse` Rework (#107)
    * to_hubverse rework
    
    * respond to reviewer comments
    
    * add more tests

--- R/to_hubverse.R
+++ R/to_hubverse.R
@@ -5,14 +5,11 @@
 #' @param reference_date hub forecasting reference date
 #' @param horizons Vector of forecast horizons to include,
 #' in weeks ahead of the `reference_date`.
-#' Default -1:3 (FluSight and Covidhub 2024/25 horizons).
-#' @return a [`tibble`][tibble::tibble] with columns `reference_date`,
-#' `horizon`, `target_end_date`,
-#' `epiweek`, and `epiyear`.
+#' @return a vector of target end dates
 #' @param horizon_timescale Either "days" or "weeks"
 #' @export
 target_end_dates_from_horizons <- function(reference_date,
-                                           horizons = -1:3,
+                                           horizons,
                                            horizon_timescale =
                                              c("days", "weeks")) {
   rlang::arg_match(horizon_timescale)
@@ -21,16 +18,9 @@ target_end_dates_from_horizons <- function(reference_date,
     days = lubridate::days(horizons),
     weeks = lubridate::weeks(horizons)
   )
+  target_end_dates <- lubridate::as_date(reference_date) + time_to_add
 
-  return(tibble::tibble(
-    reference_date = reference_date,
-    horizon = horizons,
-    target_end_date = lubridate::as_date(.data$reference_date) +
-      time_to_add,
-    epiweek = lubridate::epiweek((.data$target_end_date)),
-    epiyear = lubridate::epiyear((.data$target_end_date)),
-    horizon_timescale = !!horizon_timescale
-  ))
+  return(target_end_dates)
 }
 
 
@@ -45,22 +35,21 @@ target_end_dates_from_horizons <- function(reference_date,
 horizons_from_target_end_dates <- function(reference_date,
                                            target_end_dates,
                                            horizon_timescale) {
-  horizons <- lubridate::time_length(target_end_dates - reference_date,
+  horizons <- lubridate::time_length(
+    as.Date(target_end_dates) - as.Date(reference_date),
     unit = horizon_timescale
   )
   checkmate::assert_integerish(horizons)
   return(horizons)
 }
 
-#' Given a set of epiweekly quantile forecasts,
-#' format them for a hubverse submission
+#' Format quantile forecasts for a hubverse submission
 #'
 #' @param quantile_forecasts tidy data frame of
 #' quantile forecasts to format,
 #' which should have a location column, a column of
 #' quantile values, a column of quantile levels,
-#' a column indicating the epiweek, and a column
-#' indicating the epiyear.
+#' and columns indicating the target end date.
 #' @param reference_date reference date for the hub forecast.
 #' @param target_name Name of the target for the table.
 #' @param quantile_value_col name of the column containing the quantile
@@ -71,89 +60,82 @@ horizons_from_target_end_dates <- function(reference_date,
 #' column name from [trajectories_to_quantiles()]).
 #' @param location_col name of the column containing the location
 #' value. Default `'location'`.
-#' @param timepoint_cols names of the columns indicating the target end date.
-#' Must be some subset of `c("target_end_date", "epiweek", "epiyear")`
+#' @param timepoint_col names of the columns indicating the target end date.
+#' @param target_end_dates Vector of target end dates to include,
 #' @param horizons Vector of forecast horizons to include,
-#' in weeks ahead of the `reference_date`.
-#' Default -1:3 (FluSight and Covidhub 2024/25 horizons).
+#' in `horizon_timescale`s ahead of the `reference_date`.
 #' @param horizon_timescale Either "days" or "weeks"
-#' @param reference_dow Which day of the week should the reference_date
-#' be, as an integer? Default 7 (the last day of the week), which
-#' with the default value of `week_start` will be Saturday, i.e.
-#' the last day of a USA epidemiological week.
-#' @param week_start Starting day of the week relative to
-#' Monday, as an integer. Default 7, i.e. weeks that start on
-#' Sunday, as USA epidemiological weeks do. See the [lubridate::wday()]
-#' documentation for details.
 #' @param excluded_locations locations to exclude from the table
 #' Default c("60", "78") (American Samoa and the US Virgin Islands,
 #' for which FluSight does not currently accept forecasts).
+#' @param quantile_tol Round quantile level values to this many digits
 #' @return a properly formatted table, as a [`tibble`][tibble::tibble].
+#' @name get_hubverse_quantile_table
 #' @export
-get_hubverse_table <- function(quantile_forecasts,
-                               reference_date,
-                               target_name,
-                               quantile_value_col = "quantile_value",
-                               quantile_level_col = "quantile_level",
-                               location_col = "location",
-                               timepoint_cols = "target_end_date",
-                               horizons = -1:3,
-                               horizon_timescale =
-                                 c("days", "weeks"),
-                               reference_dow = 7,
-                               week_start = 7,
-                               excluded_locations = c("60", "78")) {
+get_hubverse_quantile_table <- function(quantile_forecasts,
+                                        reference_date,
+                                        target_name,
+                                        quantile_value_col = "quantile_value",
+                                        quantile_level_col = "quantile_level",
+                                        location_col = "location",
+                                        timepoint_col = "target_end_date",
+                                        target_end_dates = NULL,
+                                        horizons = NULL,
+                                        horizon_timescale =
+                                          c("days", "weeks"),
+                                        excluded_locations = NULL,
+                                        quantile_tol = 4) {
   rlang::arg_match(horizon_timescale)
 
-  dow_supplied <- lubridate::wday(reference_date,
-    week_start = week_start,
-    label = FALSE
+  checkmate::assert_names(colnames(quantile_forecasts),
+    must.include = timepoint_col
   )
-  reference_dow_correct <- dow_supplied == reference_dow
-  if (!reference_dow_correct) {
-    cli::cli_abort(message = paste0(
-      "Expected `reference_date` to be day number {reference_dow} ",
-      "of the week. Got {reference_date}, which is day number ",
-      "{dow_supplied} of the week, given the provided `week_start` ",
-      "value {week_start}."
-    ))
+
+  if (is.null(target_end_dates) && is.null(horizons)) {
+    cli::cli_abort(
+      message =
+        "Either `target_end_dates` or `horizons` must be provided."
+    )
+  } else if (!is.null(target_end_dates) && !is.null(horizons)) {
+    cli::cli_abort(
+      message =
+        "Only one of `target_end_dates` or `horizons` can be provided."
+    )
+  } else if (is.null(target_end_dates)) {
+    target_end_dates <- target_end_dates_from_horizons(reference_date,
+      horizons = horizons,
+      horizon_timescale = horizon_timescale
+    )
+  } else {
+    horizons <- horizons_from_target_end_dates(reference_date,
+      target_end_dates = target_end_dates,
+      horizon_timescale = horizon_timescale
+    )
   }
 
-  targets <- target_end_dates_from_horizons(
-    reference_date,
-    horizons = horizons,
-    horizon_timescale = horizon_timescale
-  ) |>
-    dplyr::mutate(
-      target = !!target_name
-    )
-
-  checkmate::assert_names(colnames(quantile_forecasts),
-    must.include = timepoint_cols
+  horizon_date_tbl <- tibble::tibble(
+    !!timepoint_col := target_end_dates,
+    "horizon" = horizons
   )
 
-  checkmate::assert_names(timepoint_cols,
-    subset.of = c("target_end_date", "epiweek", "epiyear")
-  )
-
-
-  quants <- quantile_forecasts |>
+  output_tbl <- quantile_forecasts |>
     dplyr::select(
       value = {{ quantile_value_col }},
       location = {{ location_col }},
-      tidyselect::all_of(timepoint_cols),
+      tidyselect::all_of(timepoint_col),
       quantile_level = {{ quantile_level_col }}
     ) |>
-    dplyr::filter(!(.data$location %in% !!excluded_locations))
-
-  output_table <- dplyr::inner_join(targets,
-    quants,
-    by = timepoint_cols
-  ) |>
+    dplyr::mutate(
+      "reference_date" = reference_date,
+      "target" = target_name,
+      "horizon_timescale" = horizon_timescale
+    ) |>
+    dplyr::inner_join(horizon_date_tbl, by = timepoint_col) |>
+    dplyr::filter(!(.data$location %in% !!excluded_locations)) |>
     dplyr::mutate(
       output_type = "quantile",
       output_type_id = round(.data$quantile_level,
-        digits = 4
+        digits = quantile_tol
       )
     ) |>
     dplyr::select(
@@ -176,5 +158,109 @@ get_hubverse_table <- function(quantile_forecasts,
       .data$output_type,
       .data$output_type_id
     )
-  return(output_table)
+
+  return(output_tbl)
+}
+
+
+#' @param reference_dow Which day of the week should the reference_date
+#' be, as an integer?
+#' @param week_start Starting day of the week relative to
+#' Monday, as an integer. See the [lubridate::wday()] documentation for details.
+#' @rdname get_hubverse_quantile_table
+#' @export
+get_epiweekly_hubverse_table <- function(quantile_forecasts,
+                                         reference_date,
+                                         target_name,
+                                         quantile_value_col = "quantile_value",
+                                         quantile_level_col = "quantile_level",
+                                         location_col = "location",
+                                         timepoint_col = "target_end_date",
+                                         target_end_dates = NULL,
+                                         horizons = NULL,
+                                         reference_dow = NULL,
+                                         week_start = NULL,
+                                         excluded_locations = NULL,
+                                         quantile_tol = 4) {
+  dow_supplied <- lubridate::wday(reference_date,
+    week_start = week_start,
+    label = FALSE
+  )
+  reference_dow_correct <- dow_supplied == reference_dow
+  if (!reference_dow_correct) {
+    cli::cli_abort(message = paste0(
+      "Expected `reference_date` to be day number {reference_dow} ",
+      "of the week. Got {reference_date}, which is day number ",
+      "{dow_supplied} of the week, given the provided `week_start` ",
+      "value {week_start}."
+    ))
+  }
+
+  get_hubverse_quantile_table(quantile_forecasts,
+    reference_date,
+    target_name,
+    quantile_value_col,
+    quantile_level_col,
+    location_col,
+    timepoint_col,
+    target_end_dates,
+    horizons,
+    horizon_timescale = "weeks",
+    excluded_locations,
+    quantile_tol
+  )
+}
+
+#' @rdname get_hubverse_quantile_table
+#' @export
+get_flusight_hub_table <- function(quantile_forecasts,
+                                   reference_date,
+                                   target_name,
+                                   quantile_value_col =
+                                     "quantile_value",
+                                   quantile_level_col = "quantile_level",
+                                   location_col = "location",
+                                   timepoint_col = "target_end_date",
+                                   quantile_tol = 4) {
+  get_epiweekly_hubverse_table(
+    quantile_forecasts,
+    reference_date,
+    target_name,
+    quantile_value_col,
+    quantile_level_col,
+    location_col,
+    timepoint_col,
+    horizons = -1:3,
+    reference_dow = 7,
+    week_start = 7,
+    excluded_locations = c("60", "78"),
+    quantile_tol
+  )
+}
+
+#' @rdname get_hubverse_quantile_table
+#' @export
+get_covid_hub_table <- function(quantile_forecasts,
+                                reference_date,
+                                target_name,
+                                quantile_value_col =
+                                  "quantile_value",
+                                quantile_level_col = "quantile_level",
+                                location_col = "location",
+                                timepoint_col = "target_end_date",
+                                quantile_tol = 4) {
+  get_epiweekly_hubverse_table(
+    quantile_forecasts,
+    reference_date,
+    target_name,
+    quantile_value_col,
+    quantile_level_col,
+    location_col,
+    timepoint_col,
+    horizons = -1:3,
+    reference_dow = 7,
+    week_start = 7,
+    excluded_locations = c("60", "78"),
+    quantile_tol
+  )
 }
--- tests/testthat/test_to_hubverse.R
+++ tests/testthat/test_to_hubverse.R
@@ -1,36 +1,45 @@
 quantile_forecasts <- example_daily_forecast_flu |>
   trajectories_to_quantiles(
     id_cols = "location",
-    timepoint_cols = "date",
+    timepoint_col = "date",
     value_col = "hosp"
   ) |>
   dplyr::rename(target_end_date = date)
 
 test_that(
   paste0(
-    "get_hubverse_table errors if reference date ",
+    "get_epiweekly_hubverse_table errors if reference date ",
     "is the wrong day of the week"
   ),
   {
     expect_error(
-      forecasttools::get_hubverse_table(
+      forecasttools::get_flusight_hub_table(
         tibble::tibble(),
         "2025-01-01"
       ),
       "4"
     )
     expect_error(
-      forecasttools::get_hubverse_table(
+      forecasttools::get_covid_hub_table(
+        tibble::tibble(),
+        "2025-01-01"
+      ),
+      "4"
+    )
+    expect_error(
+      forecasttools::get_epiweekly_hubverse_table(
         tibble::tibble(),
         "2025-01-01",
-        week_start = 3
+        week_start = 3,
+        reference_dow = 7
       ),
       "1"
     )
     expect_error(
-      forecasttools::get_hubverse_table(
+      forecasttools::get_epiweekly_hubverse_table(
         tibble::tibble(),
         "2025-01-01",
+        week_start = 7,
         reference_dow = 3
       ),
       "3"
@@ -38,53 +47,149 @@ test_that(
   }
 )
 
-test_that("get_hubverse_table handles different horizon_timescales correctly", {
-  expect_setequal(
-    get_hubverse_table(quantile_forecasts,
-      reference_date = "2023-10-21",
-      horizon_timescale = "days",
-      target_name = "my_target",
-      timepoint_cols = "target_end_date"
-    ) |>
-      dplyr::distinct(target_end_date) |>
-      dplyr::pull(),
-    as.Date(c("2023-10-21", "2023-10-22", "2023-10-23", "2023-10-24"))
-  )
-  expect_setequal(
-    get_hubverse_table(quantile_forecasts,
-      reference_date = "2023-10-21",
-      horizon_timescale = "weeks",
-      target_name = "my_target",
-      timepoint_cols = "target_end_date"
-    ) |>
-      dplyr::distinct(target_end_date) |>
-      dplyr::pull(),
-    as.Date(c("2023-10-21", "2023-10-28", "2023-11-04", "2023-11-11"))
-  )
-  expect_error(get_hubverse_table(quantile_forecasts,
+
+test_that(
+  "get_hubverse_quantile_table handles different horizon_timescales correctly",
+  {
+    expect_setequal(
+      object = get_hubverse_quantile_table(quantile_forecasts,
+        reference_date = "2023-10-22",
+        horizon_timescale = "days",
+        target_name = "my_target",
+        timepoint_col = "target_end_date",
+        horizons = -1:3
+      ) |>
+        dplyr::distinct(target_end_date) |>
+        dplyr::pull(),
+      expected = seq.Date(as.Date("2023-10-21"), as.Date("2023-10-25"),
+        by = "1 day"
+      )
+    )
+    expect_setequal(
+      get_hubverse_quantile_table(quantile_forecasts,
+        reference_date = "2023-10-22",
+        horizon_timescale = "days",
+        target_name = "my_target",
+        timepoint_col = "target_end_date",
+        target_end_dates = seq.Date(as.Date("2023-10-21"),
+          as.Date("2023-10-25"),
+          by = "1 day"
+        )
+      ) |>
+        dplyr::distinct(horizon) |>
+        dplyr::pull(),
+      -1:3
+    )
+
+    expect_setequal(
+      object = get_hubverse_quantile_table(quantile_forecasts,
+        reference_date = "2023-10-28",
+        horizon_timescale = "weeks",
+        target_name = "my_target",
+        timepoint_col = "target_end_date",
+        horizons = -1:3
+      ) |>
+        dplyr::distinct(target_end_date) |>
+        dplyr::pull(),
+      expected = seq.Date(as.Date("2023-10-21"),
+        as.Date("2023-11-18"),
+        by = "1 week"
+      )
+    )
+
+    expect_setequal(
+      object = get_hubverse_quantile_table(quantile_forecasts,
+        reference_date = "2023-10-28",
+        horizon_timescale = "weeks",
+        target_name = "my_target",
+        timepoint_col = "target_end_date",
+        target_end_dates = seq.Date(as.Date("2023-10-21"),
+          as.Date("2023-11-18"),
+          by = "1 week"
+        )
+      ) |>
+        dplyr::distinct(horizon) |>
+        dplyr::pull(),
+      expected = -1:3
+    )
+  }
+)
+
+test_that("get_hubverse_quantile_table errors with invalid inputs", {
+  expect_error(get_hubverse_quantile_table(quantile_forecasts,
     reference_date = "2023-10-21",
     horizon_timescale = "seconds",
     target_name = "my_target",
-    timepoint_cols = "target_end_date"
+    timepoint_col = "target_end_date"
+  ))
+  expect_error(get_hubverse_quantile_table(quantile_forecasts,
+    reference_date = "2023-10-22",
+    horizon_timescale = "days",
+    target_name = "my_target",
+    timepoint_col = "target_end_date",
+    target_end_dates = seq.Date(as.Date("2023-10-21"),
+      as.Date("2023-10-25"),
+      by = "1 day"
+    ),
+    horizons = -1:3
   ))
 })
 
+test_that(
+  "get_hubverse_quantile_table handles different horizon_timescales correctly",
+  {
+    expect_setequal(
+      get_hubverse_quantile_table(quantile_forecasts,
+        reference_date = "2023-10-21",
+        horizon_timescale = "days",
+        target_name = "my_target",
+        timepoint_col = "target_end_date",
+        horizons = -1:3
+      ) |>
+        dplyr::distinct(target_end_date) |>
+        dplyr::pull(),
+      as.Date(c("2023-10-21", "2023-10-22", "2023-10-23", "2023-10-24"))
+    )
+    expect_setequal(
+      get_hubverse_quantile_table(quantile_forecasts,
+        reference_date = "2023-10-21",
+        horizon_timescale = "weeks",
+        target_name = "my_target",
+        timepoint_col = "target_end_date",
+        horizons = -1:3
+      ) |>
+        dplyr::distinct(target_end_date) |>
+        dplyr::pull(),
+      as.Date(c("2023-10-21", "2023-10-28", "2023-11-04", "2023-11-11"))
+    )
+    expect_error(get_hubverse_quantile_table(quantile_forecasts,
+      reference_date = "2023-10-21",
+      horizon_timescale = "seconds",
+      target_name = "my_target",
+      timepoint_col = "target_end_date"
+    ))
+  }
+)
 
 
-test_that("get_hubverse_table handles improper timepoint_cols correctly", {
-  expect_error(get_hubverse_table(quantile_forecasts,
-    reference_date = "2023-10-21",
-    horizon_timescale = "days",
-    target_name = "my_target",
-    timepoint_cols = "blah"
-  ))
-  expect_error(get_hubverse_table(quantile_forecasts,
-    reference_date = "2023-10-21",
-    horizon_timescale = "days",
-    target_name = "my_target",
-    timepoint_cols = "quantile_value"
-  ))
-})
+
+test_that(
+  "get_hubverse_quantile_table handles improper timepoint_col correctly",
+  {
+    expect_error(get_hubverse_quantile_table(quantile_forecasts,
+      reference_date = "2023-10-21",
+      horizon_timescale = "days",
+      target_name = "my_target",
+      timepoint_col = "blah"
+    ))
+    expect_error(get_hubverse_quantile_table(quantile_forecasts,
+      reference_date = "2023-10-21",
+      horizon_timescale = "days",
+      target_name = "my_target",
+      timepoint_col = "quantile_value"
+    ))
+  }
+)
 
 
 reference_date <- lubridate::ymd("2025-01-01")
@@ -123,5 +228,31 @@ test_that("horizons_from_target_end_dates works correctly", {
       horizon_timescale = c("days", "weeks")
     ),
     c(1, 1)
+  )
+})
+
+test_that("target_end_dates_from_horizons works correctly", {
+  expect_equal(
+    target_end_dates_from_horizons(
+      reference_date,
+      0:(n - 1),
+      horizon_timescale = "days"
+    ),
+    seq(reference_date, by = "days", length.out = n)
+  )
+  expect_equal(
+    target_end_dates_from_horizons(
+      reference_date,
+      0:(n - 1),
+      horizon_timescale = "weeks"
+    ),
+    seq(reference_date, by = "weeks", length.out = n)
+  )
+  expect_error(
+    target_end_dates_from_horizons(
+      reference_date,
+      0:(n - 1),
+      horizon_timescale = "seconds"
+    )
   )
 })
