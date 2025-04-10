fb60e1d7cdbce21d960b0f7f6e125ebbbe6931e8
Damon Bayer <damon@damonbayer.com>
2025-04-10 15:56:43 -0500

    add modify_reference_date

---
+++ R/modify_reference_date.R
@@ -0,0 +1,60 @@
+#' Modify Reference Date
+#'
+#' @param original_hub_tbl A data frame containing the hub table.
+#' @param reference_date_transform A function to transform the reference date.
+#' Default is `identity`.
+#' @param reference_date_col The name of the reference date column. Default is
+#' "reference_date".
+#' @param target_end_date_col The name of the target end date column. Default is
+#'  "target_end_date".
+#' @param horizon_col The name of the horizon column. Default is "horizon".
+#' @param horizon_timescale_col The name of the horizon timescale column.
+#' Default is "horizon_timescale".
+#' @param horizon_timescale The timescale for the horizon. Either "days" or
+#' "weeks".
+#'
+#' @returns A modified data frame with the reference date transformed and
+#' horizons recomputed
+#' @export
+modify_reference_date <- function(original_hub_tbl,
+                                  reference_date_transform = identity,
+                                  reference_date_col = "reference_date",
+                                  target_end_date_col = "target_end_date",
+                                  horizon_col = "horizon",
+                                  horizon_timescale_col = "horizon_timescale",
+                                  horizon_timescale) {
+  checkmate::assert_names(
+    colnames(original_hub_tbl),
+    must.include = c(
+      reference_date_col,
+      target_end_date_col
+    )
+  )
+
+  optional_cols_check <- checkmate::checkNames(colnames(original_hub_tbl),
+    must.include = c(
+      horizon_col,
+      horizon_timescale_col
+    )
+  )
+
+  if (!isTRUE(optional_cols_check)) {
+    rlang::warn(paste(
+      optional_cols_check,
+      "Missing columns will be appended to the modified table.",
+      sep = "\n"
+    ))
+  }
+
+  original_hub_tbl |>
+    dplyr::mutate(
+      !!reference_date_col :=
+        reference_date_transform(.data[[reference_date_col]])
+    ) |>
+    dplyr::mutate(!!horizon_col := horizons_from_target_end_dates(
+      reference_date = .data[[reference_date_col]],
+      target_end_dates = .data[[target_end_date_col]],
+      horizon_timescale = !!horizon_timescale
+    )) |>
+    dplyr::mutate(!!horizon_timescale_col := !!horizon_timescale)
+}
---
+++ tests/testthat/test_modify_reference_date.R
@@ -0,0 +1,71 @@
+reference_date <- as.Date("2023-10-20")
+
+ex_quantiles <- example_daily_forecast_flu |>
+  trajectories_to_quantiles(
+    id_cols = "location",
+    timepoint_col = "date",
+    value_col = "hosp"
+  ) |>
+  dplyr::rename(target_end_date = date)
+
+original_hub_tbl_days <-
+  get_hubverse_quantile_table(
+    quantile_forecasts = ex_quantiles,
+    reference_date = reference_date,
+    horizon_timescale = "days",
+    target_name = "my_target",
+    timepoint_col = "target_end_date",
+    horizons = seq(7, 28, by = 7)
+  )
+
+original_hub_tbl_weeks <-
+  get_hubverse_quantile_table(
+    quantile_forecasts = ex_quantiles,
+    reference_date = reference_date,
+    horizon_timescale = "weeks",
+    target_name = "my_target",
+    timepoint_col = "target_end_date",
+    horizons = 1:4
+  )
+
+test_that("modify_reference_date converts daily to weekly horizons", {
+  modified_tbl <- modify_reference_date(
+    original_hub_tbl = original_hub_tbl_days,
+    horizon_timescale = "weeks"
+  )
+  expect_equal(modified_tbl$horizon, original_hub_tbl_days$horizon / 7)
+})
+
+test_that("modify_reference_date converts weekly to daily horizons", {
+  modified_tbl <- modify_reference_date(
+    original_hub_tbl = original_hub_tbl_weeks,
+    horizon_timescale = "days"
+  )
+  expect_equal(modified_tbl$horizon, original_hub_tbl_weeks$horizon * 7)
+})
+
+test_that("modify_reference_date warns when optional columns are missing", {
+  incomplete_tbl <- original_hub_tbl_days %>%
+    dplyr::select(-horizon, -horizon_timescale)
+
+  expect_warning(
+    modify_reference_date(
+      original_hub_tbl = incomplete_tbl,
+      horizon_timescale = "weeks"
+    ),
+    "will be appended"
+  )
+})
+
+test_that("modify_reference_date applies reference_date_transform correctly", {
+  modified_tbl <- modify_reference_date(
+    original_hub_tbl = original_hub_tbl_days,
+    reference_date_transform = function(x) as.Date(x) + 1,
+    horizon_timescale = "days"
+  )
+
+  expect_equal(
+    modified_tbl$reference_date,
+    as.Date(original_hub_tbl_days$reference_date) + 1
+  )
+})
