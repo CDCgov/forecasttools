cbaf8e35932aa3d7c34d30968ba4fb5b4447ad72
Damon Bayer <xum8@cdc.gov>
2025-08-27 13:49:57 -0500

    swap to combine_chains (#198)

--- R/inferencedata_df_to_tidy_draws.R
+++ R/inferencedata_df_to_tidy_draws.R
@@ -68,13 +68,7 @@ inferencedata_to_tidy_draws <- function(idata) {
       c(".chain", ".iteration"),
       \(x) as.integer(x + 1) # convert to 1-indexed
     )) |>
-    dplyr::mutate(
-      .draw = tidybayes:::draw_from_chain_and_iteration_(
-        .data$.chain,
-        .data$.iteration
-      ),
-      .after = ".iteration"
-    ) |>
+    tidybayes::combine_chains() |>
     tidyr::pivot_longer(
       -tidyselect::starts_with("."),
       names_sep = "\\|",
